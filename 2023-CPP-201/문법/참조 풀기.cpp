ad5dfe23d76039581ca2a1b326540e6b38bea41e
misung <voielactee7@naver.com>
2023-03-21 09:35:00 +0900

    참조로 바꾸기

--- 2023-CPP-201/문법/참조 풀기.cpp
+++ 2023-CPP-201/문법/참조 풀기.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void add_2(int* num) {
-	*num += 2;
+void add_2(int& num) {
+	num += 2;
 }
 
 int main(void) {
 	int a = 3;
-	add_2(&a);
+	add_2(a);
 
 	cout << a << endl;
 
