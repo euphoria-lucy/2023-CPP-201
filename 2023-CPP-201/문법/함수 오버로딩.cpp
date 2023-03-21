7643198dc7cda0964cf0d682c1e4b94e13a535a3
misung <voielactee7@naver.com>
2023-03-21 09:54:48 +0900

    함수 오버로딩 하기 전

---
+++ 2023-CPP-201/문법/함수 오버로딩.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int sum_int(int a, int b) {
+	return a + b;
+}
+
+float sum_float(float a, float b) {
+	return a + b;
+}
+
+int main(void) {
+	cout << sum_int(2, 9) << endl;
+	cout << sum_float(1.11, 3.3) << endl;
+	return 0;
+}
