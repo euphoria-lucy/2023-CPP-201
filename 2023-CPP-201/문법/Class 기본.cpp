bb1f07f34e79962dd1bc8c2c38de669457a99006
misung <voielactee7@naver.com>
2023-04-04 10:08:44 +0900

    멤버변수 포인터

--- 2023-CPP-201/문법/Class 기본.cpp
+++ 2023-CPP-201/문법/Class 기본.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+struct Person {
+	int weight;
+	int* height;
+};
+
 // 여러 data들을 하나의 의미있는 구조체로 묶어서 관리
 // 구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 한다.
 // class는 default가 private (struct는 default : public)
@@ -13,9 +18,6 @@ public:
 	// 생성자 (constructor) : 객체가 생성될 때 호출되는 함수
 	// 생성자를 정의하지 않으면 default(자동)로 매개변수가 없는 생성자가 정의됨
 	// Student(){} → 생성자
-
-	Student() {
-	}
 
 	Student(string _name, int _grade, int _age, int _gender, string _department) {
 		name = _name;
@@ -52,7 +54,16 @@ int main(void) {
 	
 	// (struct Student) : 자료형 (C++에서는 Student만 써도 됨)
 	// Student information = Student infromation = Student();
-	Student information = Student("임성진", 990111, 25, 0, "스포츠과학과(배구)"); // CLASS할 때 class 제외하고 코드 작성
+	Student information = Student("임성진", 990111, 25, 0, "스포츠과학과(배구)");
+
+	int h = 195;
+
+	struct Person LSJ;
+	LSJ.height = &h;
+	LSJ.weight = 85;
+
+	cout << "키 : " << LSJ.height << endl; // height가 포인터라 주소값이 출력됨
+	cout << "몸무게 : " << LSJ.weight << endl;
 
 	information.print();
 
