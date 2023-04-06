8d925da4a4384b71cc6cc3d05c0103a5edf1beaf
misung <voielactee7@naver.com>
2023-04-06 10:38:27 +0900

    → 연산자

--- 2023-CPP-201/문법/Class 기본.cpp
+++ 2023-CPP-201/문법/Class 기본.cpp
@@ -1,71 +1,76 @@
-// C++
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-struct Person {
-	int weight;
-	int* height;
+struct Actor {
+	string name;
+	string dialog;
 };
 
 // 여러 data들을 하나의 의미있는 구조체로 묶어서 관리
-// 구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 한다.
-// class는 default가 private (struct는 default : public)
+// 구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 합시다
+// class는 디폴트가 private (struct는 디폴트가 public)
 class Student {
 public:
+	// 생성자(constructor) : 객체가 생성될 때 호출되는 함수
+	Student()
+	{
+		hakbun = 990111;
+		name = "임성진";
+		age = 18;
+		sex = 0;
+		department = "스포츠과학과(배구)";
+	}
 
-	// 생성자 (constructor) : 객체가 생성될 때 호출되는 함수
-	// 생성자를 정의하지 않으면 default(자동)로 매개변수가 없는 생성자가 정의됨
-	// Student(){} → 생성자
-
-	Student(string _name, int _grade, int _age, int _gender, string _department) {
+	Student(string _name, int _hakbun, int _age, int _sex, string _department)
+	{
+		hakbun = _hakbun;
 		name = _name;
-		grade = _grade;
 		age = _age;
-		gender = _gender;
+		sex = _sex;
 		department = _department;
 	}
 
-	// class는 멤버변수를 가질 수 있음
-	void print(void) {
-		cout << "이름 : " << name << endl;
-		cout << "학번 : " << grade << endl;
-		cout << "나이 : " << age << endl;
-		cout << "성별 : " << gender << endl;
-		cout << "학과 : " << department<< endl;
+	// class는 멤버변수를 가질 수 있다
+	void print(void)
+	{
+		cout << "이름: " << name << endl;
+		cout << "학번: " << hakbun << endl;
+		cout << "나이: " << age << endl;
+		cout << "성별(0)남자, (1)여자: " << sex << endl;
+		cout << "학과: " << department << endl;
 	}
 
 private:
 	string name;
-	// 문자열로 하지 않는 이유는 성능때문에
-	// 일반적으로 문자열은 정수데이터보다 많은 메모리 공간을 요구하며 정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수만큼 반복하여 비교해야함
-	int grade;
-	int age;
-	// 열거형(enum)으로 하는 것을 추천 → 가독성과 유지보수를 위해
-	// 남자 : 0, 여자 : 1
-	int gender;
-	string department;
 
+	// 성능때문에 hakbun을 문자열로 하지 않음
+	// 일반적으로 문자열은 정수데이터보다 많은 메모리 공간을 요구하며
+	// 정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
+	int hakbun;
+	int age;
+	// 가독성과 유지보수를 위해 열거형(enum)으로 하는것을 추천
+	// 0: 남자, 1: 여자
+	int sex;
+	string department;
 };
 
 
-int main(void) {
-	
-	// (struct Student) : 자료형 (C++에서는 Student만 써도 됨)
-	// Student information = Student infromation = Student();
-	Student information = Student("임성진", 990111, 25, 0, "스포츠과학과(배구)");
+int main(void)
+{
+	Student stu1 = Student("임성진", 990111, 25, 0, "스포츠과학과 (배구)");
+	// stu1.print();
 
-	int h = 195;
+	struct Actor KDY;
+	KDY.name = "김도영";
+	KDY.dialog = "진행시켜";
 
-	struct Person LSJ;
-	LSJ.height = &h;
-	LSJ.weight = 85;
+	struct Actor* ptr = &KDY;
 
-	cout << "키 : " << *(LSJ.height) << endl; // height가 포인터라 주소값이 출력됨
-	cout << "몸무게 : " << LSJ.weight << endl;
-
-	information.print();
+	// ptr->name, (*ptr).name 같은 결과
+	cout << "이름 : " << ptr->name << endl;
+	cout << "대사 : " << (*ptr).dialog << endl;
 
 	return 0;
 }
