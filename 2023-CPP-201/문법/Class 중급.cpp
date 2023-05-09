a40f525e5a581f635a4630d5992495698546f069
misung <voielactee7@naver.com>
2023-05-09 10:17:34 +0900

    복사생성자

--- 2023-CPP-201/문법/Class 중급.cpp
+++ 2023-CPP-201/문법/Class 중급.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class MString {
 public:
-
+	// 일반적인 생성자
 	MString(const char* str) {
 		unsigned int str_length = strlen(str);
 		// 필요한 길이만큼 문자열 동적할당
@@ -14,6 +14,13 @@ public:
 		strcpy(c_str_, str);
 		cout << "MString 생성자 호출 완료" << endl;
 	} 
+
+	// 복사생성자 (별도로 정의하지 않으면 컴파일러가 아래와 같은 복사생성자를 만들어냄)
+	MString(const MString& rhs) : size_(rhs.size_), c_str_(rhs.c_str_) {
+		// size_ = rhs.size_;
+		// c_str_ = rhs.c_str_;
+		cout << "복사생성자 호출" << endl;
+	}
 
 	// 소멸자 (destructor)
 	~MString() {
@@ -31,9 +38,11 @@ private:
 
 int main(void) {
 	
-	// 동적할당 → 정적할당
+	// 일반적인 생성자 호출
 	MString str1 = MString("KIM JUNG_MIN");
-	cout << str1.c_str() << endl;
+
+	//복사생성자 호출
+	MString str2 = str1;
 
 	
 	return 0;
