01cfc8145038d1f71f6e8e2c7e4f936226eb0275
misung <voielactee7@naver.com>
2023-06-15 11:08:06 +0900

    [C++] 순수가상함수(추상메서드)

--- 2023-CPP-201/문법/상속.cpp
+++ 2023-CPP-201/문법/상속.cpp
@@ -14,10 +14,10 @@ public:
 		cout << "Animal 생성자()" << endl; 
 	}
 	virtual ~Animal() { cout << "Animal 소멸자()" << endl; }
-	// 가상함수 : Java처럼 동적바인딩
-	virtual void Roar(void) { cout << "Animal 짖다" << endl; }
-	 void Eat(void) { cout << "Animal 먹다" << endl; }
-	void Sleep(void) { cout << "Animal 자기" << endl; }
+	// 순수가상함수 : Java처럼 동적바인딩 + 추상메소드처럼 함수를 정의하지 않음 → 상속받은 자식에서 정의 (자식클래스는 무조건 정의해야함!! 아님 에러발생)
+	virtual void Roar(void) = 0;
+	virtual void Eat(void) = 0;
+	virtual void Sleep(void) = 0;
 
 private:
 	int color_;
@@ -40,11 +40,18 @@ public:
 		cout << "깡총깡총" << endl;
 	}
 
+	// ★ 순수가상함수(추상메서드)는 자식 클래스에서 무조건 정의해야함!!
+	void Eat(void) override { }
+	void Sleep(void) override { }
+
 private:
 	int ear_length_;
 };
 
 int main(void) {
+
+	// 추상클래스(순수가상함수가 하나라도 존재하는 클래스)의 객체는 생성할 수 없음!!
+	// Animal* a = new Animal(COLOR_GREEN, 10);
 
 	Animal* animal = new Rabbit(COLOR_RED, 3, 20);
 	animal->Roar(); // 깡총깡총
