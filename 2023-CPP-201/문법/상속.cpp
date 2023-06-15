ff0c644c17cb1aa651ebea191f8e86832a35fefe
misung <voielactee7@naver.com>
2023-06-15 10:49:30 +0900

    [C++] 소멸자 앞에는 무조건 virtual

--- 2023-CPP-201/문법/상속.cpp
+++ 2023-CPP-201/문법/상속.cpp
@@ -13,10 +13,10 @@ public:
 	Animal(int color, int age) : color_(color), age_(age) { 
 		cout << "Animal 생성자()" << endl; 
 	}
-	~Animal() { cout << "Animal 소멸자()" << endl; }
+	virtual ~Animal() { cout << "Animal 소멸자()" << endl; }
 	// 가상함수 : Java처럼 동적바인딩
 	virtual void Roar(void) { cout << "Animal 짖다" << endl; }
-	void Eat(void) { cout << "Animal 먹다" << endl; }
+	 void Eat(void) { cout << "Animal 먹다" << endl; }
 	void Sleep(void) { cout << "Animal 자기" << endl; }
 
 private:
@@ -31,7 +31,7 @@ public:
 	Rabbit(int color, int age, int ear_length) : Animal(color, age), ear_length_(ear_length) { 
 		cout << "Rabbit 생성자()" << endl; 
 	}
-	~Rabbit() { cout << "Rabbit 소멸자()" << endl; }
+	virtual ~Rabbit() { cout << "Rabbit 소멸자()" << endl; }
 
 	// 함수 override
 	// 다형성(polymorphism) : 시그니처(반환형, 이름, 매개변수가 모두 같은)가 같은 함수임에도 불구하고 다르게 실행되는 것
@@ -46,7 +46,6 @@ private:
 
 int main(void) {
 
-	// 소멸자에 virtual를 안하면 자식 소멸자가 호출되지 않아서 메모리 누수현상 발생
 	Animal* animal = new Rabbit(COLOR_RED, 3, 20);
 	animal->Roar(); // 깡총깡총
 	delete animal;
