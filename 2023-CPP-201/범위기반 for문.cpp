a2fe89fafe60e296970cc8c2af1e0c4775288fb5
misung <voielactee7@naver.com>
2023-10-18 09:01:00 +0900

    [C++] auto : 자료형 자동설정

--- 2023-CPP-201/범위기반 for문.cpp
+++ 2023-CPP-201/범위기반 for문.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main(void) {
 	double arr[4] = { 1.1, 2.2, 3.3, 4.4 };
 
-	for (double x : arr) {
+	// a의 자료형은 너무 명확하게 int란걸 알 수 있다.
+	auto a = 4;
+
+	// x의 자료형은 너무 명확하게 double이란걸 알 수 있다.
+	for (auto x : arr) {
 		cout << x << endl;
 	}
 
